4ffc0e38a5e274a1faf6a371fa3b75749369ce0f
ashesh <asheshmagar8888667@gmail.com>
2020-01-11 15:41:24 +0545

    commit

---
+++ notes_project/routing.py
@@ -0,0 +1,13 @@
+from channels.routing import ProtocolTypeRouter,URLRouter
+import notes.routing
+from  channels.auth import AuthMiddlewareStack
+
+
+
+application = ProtocolTypeRouter({
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            notes.routing.websocket_urlpatterns
+        )
+    )
+})
